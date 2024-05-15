bac16794abcb1505e69239d312f8acd56d1b0e68
Andrii Fedorov <sawwww7@gmail.com>
2024-05-15 20:30:31 +0300

    edits3

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const multipleOfaNumber = (i) => {
-  let multipleOfThree = i % 3 == 0;
-  let multipleOfFive = i % 5 == 0;
-  if (multipleOfThree && multipleOfFive) {
+const multipleOfaNumber = (i) => {  
+  if (i % 3 == 0 && i % 5 == 0) {
     console.log("FizzBuzz");
-  } else if (multipleOfThree) {
+  } else if (i % 3 == 0) {
     console.log("Fizz");
-  } else if (multipleOfFive) {
+  } else if (i % 5 == 0) {
     console.log("Buzz");
   } else {
     console.log("Not a multiple of 3 or 5");
@@ -17,16 +15,17 @@ multipleOfaNumber(10); //Buzz
 multipleOfaNumber(15); //FizzBuzz
 multipleOfaNumber(22); //Not a multiple of 3 or 5
 
-const highYearQualifier = (i) => {  
-    if (i % 4 == 0) {
-      console.log("A leap year.");
-    } else if (!Number.isInteger(i)) {
-      console.log(
-        "You made a mistake. Please enter the correct number of days in a year!"
-      );
-    } else {
-      console.log("A common year.");
-    } 
+
+const highYearQualifier = (i) => {
+  if (i % 4 == 0) {
+    console.log("A leap year.");
+  } else if (!Number.isInteger(i)) {
+    console.log(
+      "You made a mistake. Please enter the correct number of days in a year!"
+    );
+  } else {
+    console.log("A common year.");
+  }
 };
 
 highYearQualifier(2024); // A leap year.
@@ -37,33 +36,47 @@ highYearQualifier(2028); //A leap year.
 highYearQualifier(1.5); //You made a mistake. Please enter the correct number of days in a year!
 highYearQualifier(); //You made a mistake. Please enter the correct number of days in a year!
 
+
 const yourAge = (age) => {
-  let lastCharacter = age.toString().split("").pop();
+  try {
+    let cobString =("Вам" + " "+ age + " " );
+    switch (
+      (lastCharacter =
+        5 <= age && age <= 20 ? "$" : age.toString().split("").pop())
+    ) {
+      case "$":
+        console.log(cobString + "років!");
+        break;
+      case "1":
+        console.log(cobString + "рік");
+        break;
+      case "2":
+      case "3":
+      case "4":
+        console.log(cobString + "роки");
+        break;
+      case "0":
+      case "5":
+      case "6":
+      case "7":
+      case "8":
+      case "9":
+        console.log(cobString + "років");
+        break;   
 
-  switch (lastCharacter) {    
-    case "1":
-      console.log("Вам" + " " + age + " " + "рік");
-      break;
-    case "2":
-    case "3":
-    case "4":
-      console.log("Вам" + " " + age + " " + "роки");
-      break;
-    case "0":
-    case "5":
-    case "6":
-    case "7":
-    case "8":
-    case "9":
-      console.log("Вам" + " " + age + " " + "років");
-      break;
-
-    default:
-      console.log("You made a mistake. Please enter your age correctly");
+      default:
+        console.log("You made a mistake. Please enter your age correctly");
+    }
+  } catch (error) {
+    console.error("Error: " + error.message);
   }
 };
 
 yourAge(0); // Вам 0 років
+yourAge(11); // Вам 11 років!
 yourAge(21); // Вам 21 рік;
+yourAge(12); // Вам 11 років!
 yourAge(22); // Вам 22 роки;
 yourAge(66); // Вам 66 років;
+yourAge(); //  Error: Cannot read properties of undefined (reading 'toString')
+
